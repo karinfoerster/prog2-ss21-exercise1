b6417e3ce718e49e4ac8dd0639c29e864b9ead0c
karinfoerster <karin.foerster@stud.fh-campuswien.ac.at>
2022-03-14 18:46:05 +0100

    Testclass: validPassword1

--- src/test/java/at/ac/fhcampuswien/PasswordTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordTest.java
@@ -1,4 +1,17 @@
 package at.ac.fhcampuswien;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordTest {
+
+    //Password.checkPassword Tests
+    @Test
+    public void validPassword1()
+    {
+        Password password = new Password();
+
+        boolean result = password.checkPassword("Password01!!");
+
+        assertTrue(result);
+    }
 }
