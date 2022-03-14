d9d6c763caba6b27527f3feda323876ae9badf1a
karinfoerster <karin.foerster@stud.fh-campuswien.ac.at>
2022-03-14 18:02:15 +0100

    Mainclass: containsNumber

--- src/main/java/at/ac/fhcampuswien/Password.java
+++ src/main/java/at/ac/fhcampuswien/Password.java
@@ -3,13 +3,19 @@ package at.ac.fhcampuswien;
 public class Password {
     public boolean checkPassword(String password) {
         return password != null
-                && checkLength(password);
+                && checkLength(password)
+                && containsNumber(password);
 
     }
 
     public boolean checkLength(String password)
     {
         return password.length() >= 8 && password.length() <= 25;
+    }
+
+    public boolean containsNumber(String password)
+    {
+        return password.matches(".*\\d.*");
     }
 
 }
